0ea8b089b9b76f4e407b18d01c3612139869190a
MarioAu <mario.aunoncampillo@cieep.com>
2024-04-28 12:39:01 +0200

    Añadido navbar y enrutamiento
    Añadido navbar y enrutamiento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from '@angular/router';
+import { Routes, } from '@angular/router';
+import { HomeComponent } from './componentes/home/home.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { ContactComponent } from './componentes/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'home', title:'Home', component:HomeComponent},
+    {path:'login', title:'Login', component:LoginComponent},
+    {path:'contact', title:'Contact', component:ContactComponent},
+];
