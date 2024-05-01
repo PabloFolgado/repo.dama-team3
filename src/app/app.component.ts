68b831f79a178456a75f4e825391c8530dbb0dd5
FernandoRayoGarcia <147602281+FernandoRayoGarcia@users.noreply.github.com>
2024-05-01 13:32:39 +0200

    Añadido Navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CatalogoComponent } from './componentes/catalogo/catalogo.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CatalogoComponent],
+  imports: [RouterOutlet,CatalogoComponent, HomeComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
