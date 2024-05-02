d8dd7f86433351551c21959d3fb90e186e0a270f
MarioAu <mario.aunoncampillo@cieep.com>
2024-05-02 09:12:51 +0200

    arreglo routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import { Routes, } from '@angular/router';
 import { HomeComponent } from './componentes/home/home.component';
 import { LoginComponent } from './componentes/login/login.component';
@@ -8,14 +8,5 @@ export const routes: Routes = [
     {path:'home', title:'Home', component:HomeComponent},
     {path:'login', title:'Login', component:LoginComponent},
     {path:'contact', title:'Contact', component:ContactComponent},
-=======
-import { Routes } from '@angular/router';
-import { CatalogoComponent } from './componentes/catalogo/catalogo.component';
-import { HomeComponent } from './componentes/home/home.component';
 
-export const routes: Routes = [
-    {path:'', redirectTo:'/home', pathMatch:'full'},
-    {path:'home', title:'Home', component: HomeComponent},
-    {path:'catalogo', title:'Catalogo', component: CatalogoComponent}
->>>>>>> main
 ];
