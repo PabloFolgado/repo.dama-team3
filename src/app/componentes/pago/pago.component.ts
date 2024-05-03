f4c44169506279cb66974eac3122df1a5b187bec
MarioAu <mario.aunoncampillo@cieep.com>
2024-05-03 14:09:37 +0200

    arreglo carrito

--- src/app/componentes/pago/pago.component.ts
+++ src/app/componentes/pago/pago.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-pago',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './pago.component.html',
   styleUrl: './pago.component.css'
 })
