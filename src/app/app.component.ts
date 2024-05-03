cdf659a2b65208ed8903a4cc23aa249718d8419a
MarioAu <mario.aunoncampillo@cieep.com>
2024-05-03 12:43:24 +0200

    login registro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ import { RegisterComponent } from './componentes/register/register.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink,CatalogoComponent, HomeComponent, NavbarComponent, LoginComponent, FooterComponent],
+  imports: [RouterOutlet, RouterLink,CatalogoComponent, HomeComponent, NavbarComponent, LoginComponent,RegisterComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
