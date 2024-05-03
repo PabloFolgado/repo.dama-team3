cc76dc6940617d5da20258c29000ee5ebd35a620
MarioAu <mario.aunoncampillo@cieep.com>
2024-05-03 09:53:32 +0200

    Login y registro 03-05

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { CatalogoComponent } from './componentes/catalogo/catalogo.component';
 import { HomeComponent } from './componentes/home/home.component';
 import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { RegisterComponent } from './componentes/register/register.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CatalogoComponent, HomeComponent, NavbarComponent],
+  imports: [RouterOutlet,CatalogoComponent, HomeComponent, NavbarComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { CatalogoComponent } from './componentes/catalogo/catalogo.component';
 import { HomeComponent } from './componentes/home/home.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { RegisterComponent } from './componentes/register/register.component';
 
 export const routes: Routes = [
     {path:'', redirectTo:'/home', pathMatch:'full'},
     {path:'home', title:'Home', component: HomeComponent},
-    {path:'catalogo', title:'Catalogo', component: CatalogoComponent}
+    {path:'catalogo', title:'Catalogo', component: CatalogoComponent},
+    {path:'login', title:'Login', component: LoginComponent},
+    {path:'register', title:'Register', component: RegisterComponent}
+    
 ];
